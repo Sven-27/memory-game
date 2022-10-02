e07f769d4fb924b92c5799bcdc398901901c2998
Sven Notermans <sven.notermans@live.nl>
2022-10-02 17:19:12 +0200

    Create multi size field with card random placed in field

--- js/memory.js
+++ js/memory.js
@@ -5,9 +5,12 @@ const select = document.getElementById("speelveld");
 // Variabele voor de grootte van het speelveld
 let boardClass = "";
 
+// Variabele voor de dubbele kaarten ten opzichte de grootte van het
+let CardSetArray = [];
+
 // Template voor de card objects
-class Card{
-  constructor(card1, card2=card1, set=card1, sound=card1){
+class Card {
+  constructor(card1, card2 = card1, set = card1, sound = card1) {
     this.card1 = card1;
     this.card2 = card2;
     this.set = set;
@@ -19,54 +22,59 @@ const myField = document.getElementById("field");
 
 // lijst met de namen van afbeeldingen
 const myCardArray = ["duck", "kitten", "piglet", "puppy", "calf", "veal", "lamb", "rooster", "horse", "mouse", "dog", "cat", "goose", "goat", "sheep", "pig", "cow", "chick", "hen"];
-console.log(myCardArray.length)
-// Het aantal kaarten verdubbelen zodat elke kaart 2 keer op het scherm komt. 
-const myDblCardArray = myCardArray.concat(...myCardArray)
-console.log(myDblCardArray)
-
-
 
 // Functie om de kaarten willekeurig te schudden.
-function fyShuffle(array){
-let i = array.length;
+function fyShuffle(array) {
+  let i = array.length;
   while (--i > 0) {
     let randIndex = Math.floor(Math.random() * (i + 1));
     [array[randIndex], array[i]] = [array[i], array[randIndex]];
   }
-    return array;
+  return array;
 }
 
 // kaarten worden willekeurig neergelegd
 fyShuffle(myCardArray);
 
-// images returnen als class objecten
-const myCardSet = myCardArray.map(card => new Card(card));
-
-// Nieuwe elementen creeren om images op het scherm te tonen
-function onSelectFieldSize(e){
-  switch(e.target.value){
+// Selecteren van de grootte van het speelveld
+function onSelectFieldSize(e) {
+  // let CardSetArray = [];
+  switch (e.target.value) {
     case "4":
       boardClass = "board4";
-      console.log(boardClass = "board4");
+      CardSetArray = myCardArray.slice(0, 8).concat(...myCardArray.slice(0, 8))
       break;
     case "5":
       boardClass = "board5";
-      console.log(boardClass = "board5");
+      CardSetArray = myCardArray.slice(0, 12).concat(...myCardArray.slice(0, 12))
       break;
     case "6":
       boardClass = "board6";
-      console.log(boardClass = "board6");
+      CardSetArray = myCardArray.slice(0, 18).concat(...myCardArray.slice(0, 18))
       break;
     default:
       boardClass = "";
   }
+  fyShuffle(CardSetArray)
+  console.log(CardSetArray)
   // call functie om de kaarten op het scherm te tonen
   populateField();
 }
 
+// const myDblCardArray = myCardArray.slice(0, 8).concat(...myCardArray)
+
+// console.log(doubleCards)
+// Het aantal kaarten verdubbelen zodat elke kaart 2 keer op het scherm komt. 
+// console.log(myDblCardArray)
+
+// images returnen als class objecten
+// const myCardSet = CardSetArray.map(card => new Card(card));
+
+// Nieuwe elementen creeren om images op het scherm te tonen
+
 function populateField() {
   // Loop door de array met kaarten en maak voor elke kaart een nieuw element aan.
-  myCardSet.forEach(card => {
+  CardSetArray.map(card => new Card(card)).forEach(card => {
     // creeer container elementen voor de kaarten
     let newTile = document.createElement("div");
     // Voeg een class aan het element toe voor styling
@@ -75,7 +83,6 @@ function populateField() {
     let newCard = document.createElement("img");
     // variable die de url van de afbeeldingen als waarde heeft
     let imageURL = `img/${card.card1}.jpg`;
-    console.log(imageURL);
     // Voeg de image url van de afbeeldingen toe aan de src attribuut van de img elementen
     newCard.setAttribute("src", imageURL);
     // Naam van het dier toevoegen aan de img elementen
@@ -99,7 +106,7 @@ function populateField() {
 // Verwijder cover als op kaart geklikt wordt
 function onClickCard(e) {
   // check of classname gelijk is aan covered
-  if(e.target.className === "covered"){
+  if (e.target.className === "covered") {
     // verander classname naar uncovered
     e.target.className = "uncovered";
     //Loggen in console welke naam er verschijnt als je op een kaart klikt
